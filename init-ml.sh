5569913592c9e495dd75425d3ead9350dee03e2b
M4Tv3Y <m.tymchenko@protonmail.ch>
2022-04-05 18:59:31 +0200

    added ml init

---
+++ init-ml.sh
@@ -0,0 +1,8 @@
+echo "Starting Jupyter"
+source ~/miniconda3/etc/profile.d/conda.sh
+conda init 
+conda activate base
+echo "Doing into directory"
+cd /mnt/c/Users/mt/Documents/ML/exo
+jupyter notebook --port=8889 --no-browser
+echo "End of Script"
