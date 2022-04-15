c06bea976e6dc1e280226da72ad8ea14121b3551
M4Tv3Y <m.tymchenko@protonmail.ch>
2022-04-15 10:15:47 +0200

    modified user

--- init-ml.sh
+++ init-ml.sh
@@ -3,6 +3,6 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda init 
 conda activate base
 echo "Doing into directory"
-cd /mnt/c/Users/mt/Documents/ML/exo
+cd /mnt/c/Users/user/Documents/ML/exo
 jupyter notebook --port=8889 --no-browser
 echo "End of Script"
