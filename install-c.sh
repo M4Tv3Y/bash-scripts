a50fcfd9a949bb26f82eca4574f17b33380049b3
M4Tv3Y <m.tymchenko@protonmail.ch>
2022-04-28 13:18:14 +0200

    added another package to install

--- install-c.sh
+++ install-c.sh
@@ -5,7 +5,7 @@ sudo apt-get upgrade -y
 sudo apt-get full-upgrade -y
 echo "Install requirements"
 sudo apt install build-essential clang check wdiff colordiff git openssh-client manpages manpages-dev doxygen curl -y
-sudo apt install libssl-dev libssl-doc libcurl4-openssl-dev libjson-c-dev -y
+sudo apt install pkg-config libssl-dev libssl-doc libcurl4-openssl-dev libjson-c-dev -y
 sudo apt install make gcc clang-tools check gdb cmake -y
 sudo apt install valgrind
 sudo apt install docker docker.io -y
