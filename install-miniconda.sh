5d746925ff23d00cbcfb69d3a50b8b5522f29c3a
M4Tv3Y <m.tymchenko@protonmail.ch>
2022-04-29 02:38:12 +0200

    removed the sh and used curl

--- install-miniconda.sh
+++ install-miniconda.sh
@@ -4,6 +4,9 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get full-upgrade -y
 echo "Installing miniconda"
-chmod +x src/Miniconda3-py38_4.11.0-Linux-x86_64.sh
-./src/Miniconda3-py38_4.11.0-Linux-x86_64.sh
+cd src
+curl https://repo.anaconda.com/miniconda/Miniconda3-py38_4.11.0-Linux-x86_64.sh --output miniconda.sh
+chmod +x miniconda.sh
+./miniconda.sh
+rm -rf miniconda.sh
 echo "End of scriptx"
