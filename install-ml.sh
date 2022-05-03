89fe5d45884da80c61dd64e846827337db6dd811
M4Tv3Y <m.tymchenko@protonmail.ch>
2022-05-03 10:44:37 +0200

    Added pytorch to ml install

--- install-ml.sh
+++ install-ml.sh
@@ -2,3 +2,4 @@ conda config --set auto_activate_base false
 cowsay "Installing requirements for conda"
 conda init
 conda install --yes --file src/requirements.txt
+conda install -y pytorch torchvision -c pytorch
