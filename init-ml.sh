2df621a29c366f2b9dab7f2870a363fe8693efb1
M4Tv3Y <m.tymchenko@protonmail.ch>
2022-08-16 21:45:42 +0200

    removed init

--- init-ml.sh
+++
@@ -1,8 +0,0 @@
-cowsay "Starting Jupyter"
-source ~/miniconda3/etc/profile.d/conda.sh
-conda init 
-conda activate base
-cowsay "Doing into directory"
-cd /mnt/c/Users/user/Documents/ML/exo
-jupyter notebook --port=8889 --no-browser
-cowsay "End of Script"
